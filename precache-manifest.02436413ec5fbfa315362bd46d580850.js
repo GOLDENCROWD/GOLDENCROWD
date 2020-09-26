f7477c7c34bde65b8725ad3fadfc944592a1c75c
Jithinksatheesh <jithinksatheesh@gmail.com>
2020-09-26 21:09:02 +0530

    Updates

---
+++ precache-manifest.02436413ec5fbfa315362bd46d580850.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c806942a4117f1a11dcacae9786498",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc8144adf626d043d524",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "23757797f2d8fb081af9",
+    "url": "/static/css/main.91f75196.chunk.css"
+  },
+  {
+    "revision": "dc8144adf626d043d524",
+    "url": "/static/js/2.c504a7fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c504a7fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23757797f2d8fb081af9",
+    "url": "/static/js/main.5d38f1e8.chunk.js"
+  },
+  {
+    "revision": "183db94de12628515374",
+    "url": "/static/js/runtime-main.f0911912.js"
+  },
+  {
+    "revision": "7880748889f9ef53aba316de82f82406",
+    "url": "/static/media/hero1.78807488.jpg"
+  },
+  {
+    "revision": "4ffc1c50e2e78f98e23891991d566f66",
+    "url": "/static/media/hero2.4ffc1c50.jpg"
+  },
+  {
+    "revision": "85c6996a3d68a1899ac58f2accf6882e",
+    "url": "/static/media/jks.85c6996a.jpg"
+  },
+  {
+    "revision": "cf16bb99f8f95d02fdf2456f8dfaccf5",
+    "url": "/static/media/jp.cf16bb99.jpg"
+  },
+  {
+    "revision": "8b6906767fb1be35731690bb7ae5e42d",
+    "url": "/static/media/mg.8b690676.jpg"
+  },
+  {
+    "revision": "3d2f3e23f4b69b45ce8b198eef8ae807",
+    "url": "/static/media/parallax-footer.3d2f3e23.jpg"
+  },
+  {
+    "revision": "8b7bfe2db9ccadad7f91b88bc6f8ee0c",
+    "url": "/static/media/parallax.8b7bfe2d.jpg"
+  },
+  {
+    "revision": "89de426b2dc43c38a0ff49e0748b6026",
+    "url": "/static/media/parallaxMac.89de426b.jpg"
+  },
+  {
+    "revision": "75b915ca8822d31bb72a7a42e83a927b",
+    "url": "/static/media/parallaxYellow.75b915ca.jpg"
+  },
+  {
+    "revision": "b32780aa91fb072718edbaa5ef0d657a",
+    "url": "/static/media/team_code.b32780aa.jpg"
+  }
+]);
